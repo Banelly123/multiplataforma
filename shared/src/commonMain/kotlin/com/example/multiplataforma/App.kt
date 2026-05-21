b0b9f6807f7e59849bb9c1f2a308378a7271f741
asa20042hernandez-hub <asa20042hernandez@gmail.com>
2026-05-21 00:04:03 -0600

    Cambio 2 rama1

--- shared/src/commonMain/kotlin/com/example/multiplataforma/App.kt
+++ shared/src/commonMain/kotlin/com/example/multiplataforma/App.kt
@@ -32,7 +32,7 @@ fun App() {
             horizontalAlignment = Alignment.CenterHorizontally,
         ) {
             Button(onClick = { showContent = !showContent }) {
-                Text("holap rama1")
+                Text("Segundo cambio rama1")
             }
             AnimatedVisibility(showContent) {
                 val greeting = remember { Greeting().greet() }
